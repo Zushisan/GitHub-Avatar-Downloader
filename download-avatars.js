bebc1fcecf481bb07afafdb49eaba1d0d117c248
Zushisan <74838@supinfo.com>
2018-01-22 17:29:33 +0000

    hard code getRepo for example

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,31 @@
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  // request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+  //      .on('error', function (err) {                                   // Note 2
+  //        throw err;
+  //      })
+  //      .on('response', function (response) {                           // Note 3
+  //        console.log('Response Status Code: ', response.statusCode);
+  //        console.log(response.headers['content-type']);
+  //        console.log("Image download complete.");
+
+  //      })
+  //      .pipe(fs.createWriteStream('./future.jpg'));
+
+  // ...
+}
+
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
